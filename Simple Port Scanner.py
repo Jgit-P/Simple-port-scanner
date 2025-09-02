4f5dfb0aaddc5efb789b03c1e71fc7533c374ff1
JP <pearsonjordan18@gmail.com>
2025-09-02 10:12:13 -0500

    Update Simple Port Scanner.py
    Added a list of common ports and their services, also adjusted the for loop to also print the service, prior to this commit it only printed the port number. Now it will print both the port number and the service.

--- Simple Port Scanner.py
+++ Simple Port Scanner.py
@@ -1,6 +1,32 @@
 #Importing necessary libraries
 import socket
 
+#Common ports and services
+common_ports = {
+    20: "FTP (data transfer)",
+    21: "FTP (control)",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    53: "DNS",
+    80: "HTTP",
+    110: "POP3",
+    143: "IMAP",
+    389: "LDAP",
+    443: "HTTPS",
+    445: "SMB",
+    636: "LDAPS",
+    989: "FTPS (data)",
+    990: "FTPS (control)",
+    1433: "MSSQL",
+    1521: "Oracle",
+    3306: "MySQL",
+    3389: "RDP",
+    5432: "PostgreSQL",
+    8080: "HTTP-Proxy"
+    }
+
+
 #function definition
 def scan_ports(host, start_port, end_port):
     print(f"scanning {host} from port {start_port} to {end_port}...\n")
@@ -11,7 +37,8 @@ def scan_ports(host, start_port, end_port):
         sock.settimeout(0.5) #half second timer
         result = sock.connect_ex((host, port)) #returns 0 if open
         if result == 0:
-            print(f"Port {port}: OPEN")
+            service = common_ports.get(port, "Unknown service")
+            print(f"Port {port}: OPEN ({service})")
         sock.close()
         #The else is implied, it just skips it, not outputting anything if the result is != 0
 
@@ -19,5 +46,6 @@ if __name__ == "__main__":
     target = input("Enter host to scan (e.g., '127.0.0.1' or 'scanme.nmpa.org'): ")
     start = int(input("Enter start port: "))
     end = int(input("Enter end port: "))
+
 
     scan_ports(target, start, end)
